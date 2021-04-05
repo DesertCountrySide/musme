103e35ef9870b7d3c62a7f652d623aa3e317e4ce
Vedang Yagnik <vedang@Vedangs-MacBook-Pro.local>
2021-04-04 21:02:33 -0400

    new feat

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import { FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { AiFillPlayCircle, AiFillPauseCircle, AiOutlineDownload } from 'react-icons/ai';
 
 const Content = () => {
     const secretToken = "secret_yjrwZASe4jNqg571avHhSfgDbKegqLRQ"
@@ -41,6 +42,20 @@ const Content = () => {
           }
     },[])
 
+    const playAudio = (item) => {
+        try {
+            console.log(item);
+        //   const response = await axiosClient.get(`api/getaudio/id/${id}`)
+        //   const mp3 = new Blob([response.data], { type: 'audio/mp3' })
+        //   const url = window.URL.createObjectURL(mp3)
+        //   const audio = new Audio(url)
+        //   audio.load()
+        //   await audio.play()
+        } catch (e) {
+        //   console.log('play audio error: ', e)
+        }
+      }
+
     return (
         <ScrollView style={styles.list}>
             <FlatList
@@ -51,18 +66,31 @@ const Content = () => {
             keyExtractor={result => result.name}
             renderItem={({item}) => {
                 return (
-                    <View>
-                        <TouchableOpacity style={styles.row}>
-                            <Text numberOfLines={2} style={styles.nameTxt}>
-                                {item.name}
-                                <Text numberOfLines={2} style={styles.moodTxt}>
-                                    {(item.attributes[0] != undefined) ? `[Mood: ${item.attributes[0].value}]` : "[Mood: None]"}
-                                </Text>
-                            </Text>
+                    <View style={styles.row}>
+                        <View style={{width:500}}>
+                            <TouchableOpacity style={styles.atRow} onclick={()=>playAudio(item)}>
+                                {/* <AiFillPauseCircle style={{fontSize:30}}/> */}
+                                <AiFillPlayCircle style={{fontSize:30}} />
+                                <View style={[styles.atColumn], {marginLeft:10}}>
+                                    <Text numberOfLines={2} style={styles.nameTxt}>
+                                        {item.name}
+                                    </Text>
+                                    <Text numberOfLines={2} style={styles.moodTxt}>
+                                        {(item.attributes[0] != undefined) ? `[Mood: ${item.attributes[0].value}]` : "[Mood: None]"}
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
                             <Text numberOfLines={1}>
                                 {(item.attributes[1] != undefined) ? item.attributes[1].value : "00.00"}
                             </Text>
-                        </TouchableOpacity>
+                        </View>
+                        <View>
+                            <TouchableOpacity>
+                                <AiOutlineDownload style={{fontSize:30}} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 );
             }}
@@ -86,11 +114,18 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         paddingVertical: 10,
-        // textAlign: "left",
+        alignItems: 'center',
         paddingLeft: 10,
         borderColor: "black",
         borderBottomWidth: 1,
     },
+    atRow:{
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    atColumn:{
+        flexDirection: 'column',
+    }
   });
 
 export default Content;
--- src/TopNavigationBar.js
+++ src/TopNavigationBar.js
@@ -2,24 +2,44 @@
 
 import React, { useState } from 'react';
 import { FlatList, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Multiselect } from 'multiselect-react-dropdown';
 
 const TopNavigationBar = () => {
     const [search, setSearch] = useState("")
 
     return (
         <View style={styles.mainView}>
-            <Image 
-                style={styles.logo}
-                source={{
-                    uri: "https://i.ibb.co/4jwZbFD/musme-logo.png"
-                }}
-            />
-            <TextInput
-                style = {styles.input}
-                value = {search}
-                onChangeText = {(val) => setSearch(val)}
-                placeholder = "Enter Search term"
-            />
+            <View>
+                <Image 
+                    style={styles.logo}
+                    source={{
+                        uri: "https://i.ibb.co/4jwZbFD/musme-logo.png"
+                    }}
+                />
+            </View>
+            <View style={styles.atRow}>
+                <View style={styles.atRow}>
+                    <TextInput
+                        style = {styles.input}
+                        value = {search}
+                        onChangeText = {(val) => setSearch(val)}
+                        placeholder = "Enter Search term"
+                    />
+                    <TouchableOpacity>
+                        <Text>Search</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Multiselect style = {styles.input}
+                        options={[{name: 'Srigar', id: 1},{name: 'Sam', id: 2}]} // Options to display in the dropdown
+                        // selectedValues={this.state.selectedValue} // Preselected value to persist in dropdown
+                        showCheckbox="true"
+                        // onSelect={this.onSelect} // Function will trigger on select event
+                        // onRemove={this.onRemove} // Function will trigger on remove event
+                        displayValue="name" // Property name to display in the dropdown options
+                    />
+                </View>
+            </View>
         </View>
     );
 }
@@ -39,6 +59,13 @@ const styles = StyleSheet.create({
         margin: 10,
         borderWidth: 1,
     },
+    atRow:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    atColumn:{
+        flexDirection: 'column',
+    }
   });
 
 export default TopNavigationBar;
